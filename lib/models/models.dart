ab0bd533bfb59181ea8e56e0c4aba70ad8f36ff2
Elias Gamboa <sailegamboa@gmail.com>
2022-07-05 19:44:00 -0500

    practica 2

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,1 +1,7 @@
+export 'package:widgetgallery/models/country.dart';
+
+export 'package:widgetgallery/models/gender.dart';
+
+export 'package:widgetgallery/models/students.dart';
+
 export 'package:widgetgallery/models/item_menu.dart';
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,1 +1,7 @@
+export 'package:widgetgallery/widgets/dropdown_form.dart';
+
+export 'package:widgetgallery/widgets/bottom_menu.dart';
+export 'package:widgetgallery/widgets/genders_control.dart';
+export 'package:widgetgallery/widgets/input_form.dart';
+
 export 'package:widgetgallery/widgets/buttom_menu.dart';
